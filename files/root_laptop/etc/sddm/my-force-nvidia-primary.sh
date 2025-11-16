4b96905745008c8ae03d7f19ea32c7081fe29607
schklom <23263086+schklom@users.noreply.github.com>
2025-11-16 16:29:49 +0000

    revert nvidia as primary, it is too slow

--- files/root_laptop/etc/sddm/my-force-nvidia-primary.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# https://docs.fedoraproject.org/en-US/quick-docs/set-nvidia-as-primary-gpu-on-optimus-based-laptops/
-# value taken from `xrandr --listproviders`
-xrandr --setprovideroutputsource modesetting NVIDIA-G0
-xrandr --auto

e681fe995f60a09e43b1f49e4d1f996ee43f52b9
schklom <23263086+schklom@users.noreply.github.com>
2026-05-07 12:07:09 +0100

    don't install rpmfusion-free-release-tainted anymore, it crashes the build when on a lower version than other rpmfusion packages from fedora updates
    instead, install libdvdcss manually

---
+++ files/scripts/install-libdvdcss.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# https://blue-build.org/reference/modules/script/
+
+# set -euo pipefail -> makes the image build fail if the script fails
+
+# ${OS_VERSION} = 44
+# ${OS_ARCH} = x86_64
+
+
+package_name=`curl -s "https://download1.rpmfusion.org/free/fedora/tainted/${OS_VERSION}/${OS_ARCH}/l/" | grep -oE "libdvdcss.*?rpm" | grep -v "devel" | cut -d ">" -f 1`
+package_url="https://download1.rpmfusion.org/free/fedora/tainted/${OS_VERSION}/${OS_ARCH}/l/${package_name}"
+
+rpm-ostree install "${package_url}"
